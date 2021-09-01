f9e0493ca2528e2541ba45996d9d18778b42dfb0
Hemanth Vemulapalli <hemanthvemulapalli@gmail.com>
2021-08-31 17:07:17 -0700

    Update createUsers.ps1

--- Modules/ARM/UserCreation/scripts/createUsers.ps1
+++ Modules/ARM/UserCreation/scripts/createUsers.ps1
@@ -249,4 +249,4 @@ LogInfo("## 5 - Removing Custom Script Extension")
 
 Install-Module Az -Force -AllowClobber -Verbose
 Remove-AzVMCustomScriptExtension -ResourceGroupName $resourcegroupName -VMName $vmName -Name $extensionName
-Start-Sleep -Seconds 120
+Start-Sleep -Seconds 200
